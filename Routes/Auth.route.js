c67ecf20383969a5cea8503c6e75944a4b3a877e
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-07-10 23:17:30 -0600

    ADD
    Routes for the workspace

---
+++ Routes/Auth.route.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const AuthController = require('../Controllers/Auth.Controller')
+
+router.post('/register', async (req,res,next) => {
+    res.send('register.route')
+})
+
+router.post('/login', async (req,res,next) => {
+    res.send('register.route')
+})
+
+router.post('/refresh-token', async (req,res,next) => {
+    res.send('register.route')
+})
+
+router.post('/logout', async (req,res,next) => {
+    res.send('register.route')
+})
+
+/*
+
+router.post('/register', AuthController.register)
+
+router.post('/login', AuthController.login)
+
+router.post('/refresh-token', AuthController.refreshToken)
+
+router.delete('/logout', AuthController.logout)
+
+module.exports = router
+*/
