679789238a45a28268a937dd63144de190598dcc
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-07-25 00:36:41 -0600

    MODIFY
    Result variable will now manage email and password by schema

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -5,7 +5,7 @@ const { authSchema } = require ('../helpers/validation_schema')
 
 router.post('/register', async (req,res,next) => {
     try {
-        const { email, password } = req.body
+        // const { email, password } = req.body
         // if (!email || !password) throw createError.BadRequest()
         const result = await authSchema.validateAsync(req.body)
         console.log('result')
@@ -13,7 +13,7 @@ router.post('/register', async (req,res,next) => {
         const doesExist = await User.findOne({ email: result.email })
         if (doesExist)
             throw createError.Conflict(`${result.email} is already been registered`)
-        const user = new User({email, password})
+        const user = new User(result)
         const savedUser = await user.save()
         
         res.send(savedUser)
