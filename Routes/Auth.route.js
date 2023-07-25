913a64d5a216ca7b5359605cd63e63e5dc8de462
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-07-24 23:56:54 -0600

    MODIFY
    Add register user
    try catch management

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -3,7 +3,22 @@ const router = express.Router()
 const AuthController = require('../Controllers/Auth.Controller')
 
 router.post('/register', async (req,res,next) => {
-    res.send('register.route')
+    try {
+        const { email, password } = req.body
+        if (!email || !password) throw createError.BadRequest()
+      
+        const doesExist = await User.findOne({ email: result.email })
+        if (doesExist)
+            throw createError.Conflict(`${result.email} is already been registered`)
+        const user = new User({email, password})
+        const savedUser = await user.save()
+        
+        res.send(savedUser)
+        
+    } catch (error) {
+      if (error.isJoi === true) error.status = 422
+      next(error)
+    }
 })
 
 router.post('/login', async (req,res,next) => {
