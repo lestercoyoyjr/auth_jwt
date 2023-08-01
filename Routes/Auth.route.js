341fdedcca1ece0a421edcf728e182a64f2bd49a
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-08-01 00:19:25 -0600

    ADD
    Adding the Controller pattern & Clean up

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -1,86 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const AuthController = require('../Controllers/Auth.Controller')
-const { authSchema } = require ('../helpers/validation_schema')
-const { signAccessToken, signRefreshToken, verifyRefreshToken } = require('../helpers/jwt_helper')
-const createError = require('http-errors')
-const { verify } = require('jsonwebtoken')
-const client = require('../helpers/init_redis')
-
-router.post('/register', async (req,res,next) => {
-    try {
-        // const { email, password } = req.body
-        // if (!email || !password) throw createError.BadRequest()
-        const result = await authSchema.validateAsync(req.body)
-        console.log('result')
-      
-        const doesExist = await User.findOne({ email: result.email })
-        if (doesExist)
-            throw createError.Conflict(`${result.email} is already been registered`)
-        const user = new User(result)
-        const savedUser = await user.save()
-        const accessToken = await signAccessToken(savedUser.id)
-        res.send(savedUser)
-        
-    } catch (error) {
-        if (error.isJoi === true) error.status = 422
-        next(error)
-    }
-})
-
-router.post('/login', async (req,res,next) => {
-    try {
-        const result = await authSchema.validateAsync(req.body)
-        const user = await User.findOne({email : result.email})
-        if(!user) throw createError.NotFound("User not registered")
-
-        const isMatch = await user.isValidPassword(result.password)
-
-        if(!isMatch) throw createError.Unauthorized('username/password not valid')
-
-        const accessToken = await signAccessToken(user.id)
-        const refreshToken = await signRefreshToken(user.id)
-
-        res.send({accessToken})
-    } catch (error) {
-        if (error.isJoi === true) return next(createError.BadRequest("Invalid Username/Password"))
-        next(error)
-    }
-})
-
-router.post('/refresh-token', async (req,res,next) => {
-    try {
-        const { refreshToken } = req.body
-        if (!refreshToken) throw createError.BadRequest()
-        const userId = await verifyRefreshToken(refreshToken)
-  
-        const accessToken = await signAccessToken(userId)
-        const refToken = await signRefreshToken(userId)
-        res.send({ accessToken: accessToken, refreshToken: refToken })
-      } catch (error) {
-        next(error)
-      }
-})
-
-router.post('/logout', async (req,res,next) => {
-    try {
-        const {refreshToken} = req.body
-        if(!refreshToken) throw createError.BadRequest()
-        const userId = await verifyRefreshToken(refreshToken)
-        client.DEL(userId, (err, val) =>{
-            if (err){
-                console.log(err.message)
-                throw createError.InternalServerError()
-            }
-            console.log(val)
-            res.status(204)
-        })
-    } catch (error) {
-        next(error)
-    }
-})
-
-/*
 
 router.post('/register', AuthController.register)
 
@@ -91,4 +11,3 @@ router.post('/refresh-token', AuthController.refreshToken)
 router.delete('/logout', AuthController.logout)
 
 module.exports = router
-*/
